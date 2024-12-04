68cbc458605eadf161c1e415c1724085eb29ac7e
Rizky Dhani <rizkydhani15@gmail.com>
2024-12-04 13:25:45 +0700

    install laravel-livewire-tables by rappasoft

--- resources/views/livewire/contacts/contact-index.blade.php
+++ resources/views/livewire/contacts/contact-index.blade.php
@@ -1,3 +1,4 @@
+-- Active: 1730943391040@@127.0.0.1@3306@vcard
 <div><!-- ========== title-wrapper start ========== -->
     <div class="title-wrapper pt-30">
         <div class="row align-items-center">
