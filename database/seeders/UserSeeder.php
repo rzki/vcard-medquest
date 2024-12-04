91ae7694f5de67c70946adac877301f50b19c686
Rizky Dhani <rizkydhani15@gmail.com>
2024-12-04 10:44:46 +0700

    added Role and User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $superadmin = User::create([
+            'userId' => Str::orderedUuid(),
+            'name' => 'Superadmin',
+            'email' => 'superadmin@medquest.co.id',
+            'password' => Hash::make('Superadmin2024!')
+        ]);
+
+        $superadmin->assignRole('Super Admin');
     }
 }
