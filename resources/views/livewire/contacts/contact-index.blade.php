7b19176aae613e9f0ffd901c54f3ca8fe546c243
Rizky Dhani <rizkydhani15@gmail.com>
2024-12-04 11:59:07 +0700

    added Contact resource

---
+++ resources/views/livewire/contacts/contact-index.blade.php
@@ -0,0 +1,24 @@
+<div><!-- ========== title-wrapper start ========== -->
+    <div class="title-wrapper pt-30">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="title mb-30">
+                    <h2>{{ __('Contacts') }}</h2>
+                </div>
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- ========== title-wrapper end ========== -->
+
+    <div class="card-styles">
+        <div class="card-style-3 mb-30">
+            <div class="card-content">
+                <p>
+                    {{ __('You are logged in!') }}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Contacts\ContactIndex;
 use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,8 @@ Route::get('/home', Dashboard::class)->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::view('about', 'about')->name('about');
-
     Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::get('contacts', ContactIndex::class)->name('contacts.index');
 });
