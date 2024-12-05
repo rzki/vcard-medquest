5c26ad7c42b8dc1afcf6edac3c5453b501bc359d
Rizky Dhani <rizkydhani15@gmail.com>
2024-12-05 15:23:44 +0700

    added search

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     protected $guarded = ['id'];
+    public function scopeSearch($query, $value)
+    {
+        $query->where('first_name', 'like', "%{$value}%")
+                ->orWhere('last_name', 'like', "%{$value}%")
+                ->orWhere('email', 'like', "%{$value}%")
+                ->orWhere('phone_number', 'like', "%{$value}%");
+    }
 }
