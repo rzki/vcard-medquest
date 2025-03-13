5a897525962cc05fd06f0a8c651c22c25e379481
Rizky Dhani <rizkydhani15@gmail.com>
2025-03-13 11:49:31 +0700

    added update method on Edit Division component

--- app/Livewire/Divisions/DivisionEdit.php
+++ app/Livewire/Divisions/DivisionEdit.php
@@ -3,10 +3,32 @@
 namespace App\Livewire\Divisions;
 
 use Livewire\Component;
+use App\Models\Division;
 use Livewire\Attributes\Title;
 
 class DivisionEdit extends Component
 {
+    public $division, $divisionId, $name;
+    public function mount($divisionId){
+        $division = Division::find($divisionId)->first();
+        $this->name = $division->name;
+    }
+    public function update(){
+        Division::where('divisionId', $this->divisionId)->update([
+        'name' => $this->name
+    ]);
+    session()->flash('alert', [
+        'type' => 'success',
+        'title' => 'Division updated successfully!',
+        'toast' => false,
+        'position' => 'center',
+        'timer' => 1500,
+        'progbar' => false,
+        'showConfirmButton' => false,
+    ]);
+    return $this->redirectRoute('divisions.index', navigate:true);
+
+    }
     #[Title('Edit Division')]
     public function render()
     {
